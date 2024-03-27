2946739f1db08737a39d6cd099736bf89d713847
Braylin <braylinvasquez20@gmail.com>
2024-03-27 11:45:35 -0400

    AlguacilServices

---
+++ ProyectFinal/Services/AlguacilServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectFinal.Data;
+using Share.Models;
+using System.Linq.Expressions;
+
+namespace ProyectFinal.Services
+{
+    public class AlguacilServices(ApplicationDbContext _context)
+    {
+        public async Task<List<Alguaciles>> Listar(Expression<Func<Alguaciles, bool>> criterio)
+        {
+            return await _context.Alguaciles
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+    
+}
