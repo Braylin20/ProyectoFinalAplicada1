751ec5bce9ede5829a9ed021936f49eac45582db
Braylin <braylinvasquez20@gmail.com>
2024-03-27 12:37:31 -0400

    ninoDetalle

---
+++ Shared/Models/NinoDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models
+{
+    public class NinoDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int DemandaId { get; set; }
+        public string? NombreNino { get; set; }
+        public DateTime? FechaNacimientoNino { get; set; }
+        public string? Genero { get; set; }
+    }
+}
