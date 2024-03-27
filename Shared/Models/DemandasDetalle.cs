62aff7bbb8e22e21c35546e8399ce18a8b4741f6
Braylin <braylinvasquez20@gmail.com>
2024-03-27 12:37:04 -0400

    demanda detalle

---
+++ Shared/Models/DemandasDetalle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models
+{
+    public class DemandasDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int DemandaId { get; set; }
+        public string? NumCedulaDemandado { get; set; }
+        public string? NombreDemandado { get; set; }
+        //public string? NombreNino { get; set; }
+        //public DateTime? FechaNacimientoNino { get; set; }
+        //public string? Genero { get; set; }
+    }
+}
