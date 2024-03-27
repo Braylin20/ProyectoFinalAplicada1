f775f687bf1db89308b26e7afc6cde20a7cd182f
Braylin <braylinvasquez20@gmail.com>
2024-03-27 12:38:16 -0400

    uusario detalle

---
+++ Shared/Models/UsuarioDetalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models
+{
+    public class UsuarioDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public string Id { get; set; }
+        public string? NombreAbogado { get; set; }
+        public int? ColegioAbogadoId { get; set; }
+    }
+}
