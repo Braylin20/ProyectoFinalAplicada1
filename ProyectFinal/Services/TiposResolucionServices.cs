31221face9c060b44b4154ae7f4c2a653f0b9707
eliezer terrero <eliezerterrero12@gmail.com>
2024-03-28 15:55:35 -0400

    Creando el servicios de tiposResolucion

---
+++ ProyectFinal/Services/TiposResolucionServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectFinal.Data;
+using Share.Models;
+
+namespace ProyectFinal.Services
+{
+    public class TiposResolucionServices(ApplicationDbContext _context)
+    {
+
+        public async Task<List<TipoResoluciones>> GetResoluciones()
+        {
+            return await _context!.TipoResoluciones
+                .AsNoTracking().ToListAsync();
+        }
+        public string GetResolucion(int id)
+        {
+            var resolucion = _context!.TipoResoluciones.Find(id);
+
+            return resolucion != null ? resolucion.TipoResolcion : "";
+        }
+
+
+    }
+}
