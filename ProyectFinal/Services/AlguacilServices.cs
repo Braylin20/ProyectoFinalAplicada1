fbaa1fa727499f7cab9d8bc9fe0c7e368fe6d6e4
eliezer terrero <eliezerterrero12@gmail.com>
2024-03-29 17:53:37 -0400

    Agregandole metodos a AlguacilServices

--- ProyectFinal/Services/AlguacilServices.cs
+++ ProyectFinal/Services/AlguacilServices.cs
@@ -14,6 +14,41 @@ namespace ProyectFinal.Services
                 .Where(criterio)
                 .ToListAsync();
         }
+
+
+        public async Task<List<Alguaciles>> GetAlguaciles()
+        {
+            return await _context!.Alguaciles.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Alguaciles?> GetAlguacil(int id)
+        {
+            return await _context!.Alguaciles.FindAsync(id);
+        }
+        public async Task<bool> DeleteAlguacil(int id)
+        {
+            return await _context!.Alguaciles
+                .AsNoTracking()
+                .Where(a => a.AlguacilId == id)
+                .ExecuteDeleteAsync() > 0;
+
+        }
+       
+        public async Task<bool> Save(Alguaciles alguacil)
+        {
+            if (alguacil.AlguacilId == 0)
+                _context!.Alguaciles.Add(alguacil);
+            else
+                _context!.Alguaciles.Update(alguacil);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+        public async Task<bool> Edit(Alguaciles Alguacil)
+        {
+            _context!.Update(Alguacil);
+            _context.Entry(Alguacil).State = EntityState.Modified;
+            return await _context.SaveChangesAsync() > 0;
+        }
     }
     
 }
