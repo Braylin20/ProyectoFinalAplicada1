725779ce4983e3c2cf295a52194a0148a2e0d67a
Braylin <braylinvasquez20@gmail.com>
2024-03-30 17:00:12 -0400

    Arregladno detales

--- Shared/Models/ApplicationUser.cs
+++ Shared/Models/ApplicationUser.cs
@@ -10,18 +10,16 @@ namespace ProyectFinal.Data
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        //public int? ExpedienteId { get; set; }
+
         public int? AbogadoId { get; set; }
         public long? Cedula { get; set; }
         
         [Required(ErrorMessage = "Este campo es requerido")]
         public DateTime FechaCreacion { get; set; }
-        //[ForeignKey("ExpedienteId")]
-        //public Expedientes? Exepediente { get; set; }
+
         [ForeignKey("AbogadoId")]
         public UsuarioDetalle? Abogado { get; set; }
-        //[ForeignKey("NinoId")]
-        //public Niños? Niño { get; set; }
+
         [ForeignKey("Id")]
         public ICollection<UsuarioDetalle> UsuarioDetalle { get; set; } = new List<UsuarioDetalle>();
 
