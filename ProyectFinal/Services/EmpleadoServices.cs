4bc0935d679ac306371fa2a247c1d85623c74735
Braylin <braylinvasquez20@gmail.com>
2024-03-30 19:31:04 -0400

    Teerminando el index de empeleado

--- ProyectFinal/Services/EmpleadoServices.cs
+++ ProyectFinal/Services/EmpleadoServices.cs
@@ -18,15 +18,31 @@ namespace ProyectFinal.Services
                 .Where(a => a.EmpleadoId == id)
                 .ExecuteDeleteAsync() > 0;
         }
-        //public  Task<ICollection<ApplicationUser>> Listar(ICollection<ApplicationUser> empleados, ApplicationUser user)
-        //{
-        //    var users =  empleados.Where(e => e.Nombre!.ToLower().Contains(user.Nombre!.ToLower())).ToList();
-        //    return users;
-        //}
-        //public  Task<ICollection<ApplicationUser>> ListarPorFecha(ICollection<ApplicationUser> empleados,DateTime FechaInicio, DateTime FechaFinal)
-        //{
-        //    var users = empleados.FirstOrDefault(m => m.FechaCreacion.Date <= FechaFinal.Date && m.FechaCreacion.Date >= FechaInicio.Date);
-        //}
+        public Task<ICollection<ApplicationUser>> Listar(ICollection<ApplicationUser> empleados, ApplicationUser user)
+        {
+            
+            var users = empleados.Where(e => e.Nombre!.ToLower().Contains(user.Nombre!.ToLower())).ToList();
+            
+            return Task.FromResult((ICollection<ApplicationUser>)users);
+        }
+        public Task<ICollection<ApplicationUser>> ListarPorCedula(ICollection<ApplicationUser> empleados, ApplicationUser user)
+        {
+
+            ApplicationUser? userTemp = empleados?.FirstOrDefault(e => e.Cedula == user.Cedula);
+            if (userTemp == null)
+            {
+                return Task.FromResult<ICollection<ApplicationUser>>(new List<ApplicationUser>());
+            }
+            else
+            {
+                return Task.FromResult<ICollection<ApplicationUser>>(new List<ApplicationUser>() { userTemp });
+            }
+        }
+        public Task<ICollection<ApplicationUser>?> ListarPorFecha(ICollection<ApplicationUser> empleados, DateTime FechaInicio, DateTime FechaFinal)
+        {
+            var users = empleados!.Where(m => m.FechaCreacion!.Date <= FechaFinal.Date && m.FechaCreacion!.Date >= FechaInicio.Date).ToList();
+            return Task.FromResult((ICollection<ApplicationUser>?)users);
+        }
 
         public async Task<bool> Save(Empleados empleado)
         {
