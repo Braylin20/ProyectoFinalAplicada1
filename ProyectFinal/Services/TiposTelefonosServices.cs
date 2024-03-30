2c11e60786a78f02e7681e0cc66047f3bc3bb580
Braylin <braylinvasquez20@gmail.com>
2024-03-30 09:45:44 -0400

    Tipo de Telefono Services

---
+++ ProyectFinal/Services/TiposTelefonosServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectFinal.Data;
+using Share.Models;
+using System.Linq.Expressions;
+
+namespace ProyectFinal.Services
+{
+    public class TiposTelefonosServices(ApplicationDbContext _context)
+    {
+        public async Task<List<TipoTelefonos>> Listar(Expression<Func<TipoTelefonos, bool>> criterio)
+        {
+            return await _context.TipoTelefonos
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
