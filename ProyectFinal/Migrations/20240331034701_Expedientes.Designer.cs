ae91cc8d121c34e9dc08bb6787742e86c7f7d2ce
Braylin <braylinvasquez20@gmail.com>
2024-03-31 09:34:26 -0400

    Agregando la cedula en la demanda

--- ProyectFinal/Migrations/20240331034701_Expedientes.Designer.cs
+++ ProyectFinal/Migrations/20240331034701_Expedientes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectFinal.Data;
 
@@ -11,9 +12,11 @@ using ProyectFinal.Data;
 namespace ProyectFinal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240331034701_Expedientes")]
+    partial class Expedientes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
