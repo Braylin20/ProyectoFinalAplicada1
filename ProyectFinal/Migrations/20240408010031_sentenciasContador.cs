b0068758c7a1e0a1c69e0c95406a37b2593b257e
eliezer terrero <eliezerterrero12@gmail.com>
2024-04-07 23:02:55 -0400

    haciendo migracion

---
+++ ProyectFinal/Migrations/20240408010031_sentenciasContador.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class sentenciasContador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Sentencia",
+                table: "Sentencias",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ContadorSentencias",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContadorSentencias",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Sentencia",
+                table: "Sentencias",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+    }
+}
