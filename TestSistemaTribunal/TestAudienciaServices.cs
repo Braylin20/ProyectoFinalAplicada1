33c89fc0a057e77d9a3cd61f242dda0861a1ac8c
Braylin <braylinvasquez20@gmail.com>
2024-04-08 00:03:25 -0400

    Agregando testing

---
+++ TestSistemaTribunal/TestAudienciaServices.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ProyectFinal.Data;
+using ProyectFinal.Services;
+using Share.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace TestSistemaTribunal;
+
+[TestClass]
+public class TestAudienciaServices
+{
+    [TestMethod]
+    public async Task AddObject_ShouldAddNewAbonos()
+    {
+            //Arrange
+       var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+           .UseInMemoryDatabase(databaseName: "TestDatabase")
+           .Options;
+        var dbContext = new ApplicationDbContext(options);
+        var audienciaService = new AudienciasServices(dbContext);
+        // Act
+        var nuevaAudiencia = new Audiencias
+        {
+            FechaAudiencia = DateTime.Now,
+            Comentario = "Comentario de prueba"
+        };
+        var resultado = await audienciaService.Save(nuevaAudiencia);
+
+        //Assert
+        Assert.IsTrue(resultado); 
+    }
+}
---
+++ TestSistemaTribunal/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestSistemaTribunal
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
