2bb1f98b1f5f3d67839a36134f1042cd17176bfd
eliezer terrero <eliezerterrero12@gmail.com>
2024-04-08 14:15:01 -0400

    cambiando el metodo de listar

--- ProyectFinal/Services/AlguacilServices.cs
+++ ProyectFinal/Services/AlguacilServices.cs
@@ -7,14 +7,7 @@ namespace ProyectFinal.Services
 {
     public class AlguacilServices(ApplicationDbContext _context)
     {
-        public async Task<List<Alguaciles>> Listar(Expression<Func<Alguaciles, bool>> criterio)
-        {
-            return await _context.Alguaciles
-                .AsNoTracking()
-                .Where(criterio)
-                .ToListAsync();
-        }
-
+     
 
         public async Task<List<Alguaciles>> GetAlguaciles()
         {
@@ -25,15 +18,16 @@ namespace ProyectFinal.Services
         {
             return await _context!.Alguaciles.FindAsync(id);
         }
-        public async Task<bool> DeleteAlguacil(int id)
+        public async Task DeleteAlguacil(int id)
         {
-            return await _context!.Alguaciles
-                .AsNoTracking()
-                .Where(a => a.AlguacilId == id)
-                .ExecuteDeleteAsync() > 0;
-
+            var alguacil = await _context.Alguaciles.FindAsync(id);
+            if (alguacil != null)
+            {
+                _context.Alguaciles.Remove(alguacil);
+                await _context.SaveChangesAsync();
+            }
         }
-       
+
         public async Task<bool> Save(Alguaciles alguacil)
         {
             if (alguacil.AlguacilId == 0)
@@ -41,12 +35,6 @@ namespace ProyectFinal.Services
             else
                 _context!.Alguaciles.Update(alguacil);
 
-            return await _context.SaveChangesAsync() > 0;
-        }
-        public async Task<bool> Edit(Alguaciles Alguacil)
-        {
-            _context!.Update(Alguacil);
-            _context.Entry(Alguacil).State = EntityState.Modified;
             return await _context.SaveChangesAsync() > 0;
         }
     }
